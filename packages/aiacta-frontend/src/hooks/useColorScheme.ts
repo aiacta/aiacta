40eb62a81fd59b01506ce573c3150d9320646477
Jonathan Burke <jonathan.burke@codecentric.de>
2021-05-19 17:57:11 +0200

    refactor: use mantine color scheme hook

--- packages/aiacta-frontend/src/hooks/useColorScheme.ts
+++ packages/aiacta-frontend/src/hooks/useColorScheme.ts
@@ -1,3 +1,4 @@
+import { useColorScheme as useQueryColorScheme } from '@mantine/hooks';
 import * as React from 'react';
 import { atom, SetterOrUpdater, useRecoilState } from 'recoil';
 
@@ -7,19 +8,7 @@ const colorSchemeAtom = atom<'light' | 'dark' | null>({
 });
 
 export function useColorScheme() {
-  const queryRef = React.useRef<MediaQueryList>();
-  if (!queryRef.current) {
-    queryRef.current = window.matchMedia('(prefers-color-scheme: dark)');
-  }
-  const [colorScheme, setColorScheme] = React.useState(
-    queryRef.current.matches ? ('dark' as const) : ('light' as const),
-  );
-
-  React.useEffect(() => {
-    return attachMediaListener(queryRef.current!, (event) =>
-      setColorScheme(event.matches ? 'dark' : 'light'),
-    );
-  });
+  const colorScheme = useQueryColorScheme();
 
   const [appColorScheme, setAppColorScheme] = useRecoilState(colorSchemeAtom);
   const colorRef = React.useRef(appColorScheme ?? colorScheme);
@@ -35,14 +24,4 @@ export function useColorScheme() {
       );
     }) as SetterOrUpdater<'light' | 'dark'>,
   ] as const;
-}
-
-type MediaQueryCallback = (event: { matches: boolean; media: string }) => void;
-
-function attachMediaListener(
-  query: MediaQueryList,
-  callback: MediaQueryCallback,
-) {
-  query.addEventListener('change', callback);
-  return () => query.removeEventListener('change', callback);
 }
