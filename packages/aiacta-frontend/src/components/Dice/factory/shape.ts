d364945ceef7589b84b82c4145d77ceaaf177ce5
Jonathan Burke <jonathan.burke@codecentric.de>
2021-05-25 14:28:01 +0200

    refactor: simplify shape generator

--- packages/aiacta-frontend/src/components/Dice/factory/shape.ts
+++ packages/aiacta-frontend/src/components/Dice/factory/shape.ts
@@ -2,22 +2,15 @@ import { ConvexPolyhedron, Vec3 } from 'cannon-es';
 import { dice, DieType } from './dieValues';
 
 export function createShape(die: DieType) {
-  const vertices: Vec3[] = [];
+  const vertices: Vec3[] = dice[die].vertices.map(
+    (v) => new Vec3(...v.toArray()),
+  );
 
   const shape = new ConvexPolyhedron({
     faces: dice[die].faces.flatMap((f) => {
-      return f.indices.map((i) => {
-        const arr = i.map((idx) => {
-          const pos = new Vec3().copy(dice[die].vertices[idx] as any);
-          const vi = vertices.findIndex((v) => v.almostEquals(pos));
-          if (vi >= 0) {
-            return vi;
-          } else {
-            return vertices.push(pos) - 1;
-          }
-        });
-        (arr as any).faceValue = f.value;
-        return arr;
+      return f.indices.map((indices) => {
+        (indices as any).faceValue = f.value;
+        return indices;
       });
     }),
     vertices,
